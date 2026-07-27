bf9837d6c69c46b0e97cf902c33c0075ec929dde
Rocky-bhai-creator <123227420+Rocky-bhai-creator@users.noreply.github.com>
2026-07-27 18:21:10 +0530

    1st commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Home from './pages/Home/Home'
+import { Box } from '@mui/material'
 
 function App() {
   return (
-    <div>
+    <Box>
       <Home/>
-    </div>
+    </Box>
   )
 }
 
