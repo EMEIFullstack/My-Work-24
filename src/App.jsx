07afd6b33a088c377f55f8c1c19d3d7b63629b55
sagarika20-MAJUMDAR <sagarika.m2004@gmail.com>
2026-08-02 00:22:18 +0530

    Add hero section UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import { BrowserRouter as Router, Routes, Route  } from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </Router>
+    </div>
   )
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+
+
+import Hero from '../../Components/Hero/Hero';
+import Header from '../../Shared/Header/Header';
+
+export default function Home() {
+    return (
+        <div>
+            <>
+
+            <Header/>
+            <Hero/>
+            </>
+
+        </div>
+    )
+}
