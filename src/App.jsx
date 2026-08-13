500f800dff0a596bcb74e806b4f9886446d0735c
Aditi <aditidhar30@gmail.com>
2026-08-13 22:15:06 +0530

    Remove app.css and index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import { BrowserRouter as Router, Routes, Route  } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Header from "./Shared/Header/Header";
-
-
-
-
+import ServicesSection from "./pages/ServicesSection";
 import Footer from "./Shared/footer";
 
 function App() {
   return (
     <div>
-
       <Router>
-        <Header/>
+        <Header />
         <Routes>
-          <Route path='/' element={<Home/>}/>
+          <Route path='/' element={
+            <>
+              <Home />
+              <ServicesSection />
+            </>
+          } />
         </Routes>
-        <Footer/>
+        <Footer />
       </Router>
     </div>
-  )
+  );
 }
 
 export default App;
