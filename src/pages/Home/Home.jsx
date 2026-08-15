8bf3df947eac85b2d56516aec7758e9bdca0bb0f
sagarika20-MAJUMDAR <sagarika.m2004@gmail.com>
2026-08-15 22:58:34 +0530

    feat: make How It Works section dynamic

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,17 @@
 
 
 import Hero from '../../Components/Hero/Hero';
+import HowItWorks from '../../Components/HowItWork/HowItWork';
 
 
 export default function Home() {
-
-    
     return (
         <div>
             <>
 
+            
             <Hero/>
+            <HowItWorks/>
             </>
 
         </div>
