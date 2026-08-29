e77280380accd76f743500045e445d4b71eaae34
sagarika20-MAJUMDAR <sagarika.m2004@gmail.com>
2026-08-30 01:00:38 +0530

    user profile page

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,15 @@ import ServicesSection from "./pages/ServicesSection";
 import Footer from "./Shared/footer";
 import WindowCleaning from "./pages/WindowCleaning/WindowCleaning";
 import Signup from "./pages/Signup/Signup";
-import HowItWork from "./pages/HowItWork/HowItWork"
+import HowItWork from "./pages/HowItWork/HowItWork";
 import About from "./pages/About/About";
-import Contact from "./pages/Contact/Contact"
+import Contact from "./pages/Contact/Contact";
 import Subscription from "./pages/Subscription/Subscription";
 import SignUp from "./Pages/Auth/SignUp";
 import Login from "./Pages/Auth/Login";
 import ForgotPassword from "./Pages/Auth/ForgotPassword";
 import NewPassword from "./Pages/Auth/NewPassword";
+import UserProfile from "./Components/Auth/UserProfile/UserProfile";
 
 function App() {
   return (
@@ -20,49 +21,37 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path='/' element={
-            <>
-              <Home />
-              <ServicesSection />
-              <HowItWork/>
-              
-             
-            </>
-          } />
-          <Route path='/windowcleaning' element={<WindowCleaning />} />
-          <Route path="/subscription" element={<Subscription />} />
           <Route
-            path="/signup"
-            element={<Signup />}
+            path="/"
+            element={
+              <>
+                <Home />
+                <ServicesSection />
+                <HowItWork />
+              </>
+            }
           />
-                <Route path='/aboutus' element={<About />} />
+          <Route path="/windowcleaning" element={<WindowCleaning />} />
+          <Route path="/subscription" element={<Subscription />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/aboutus" element={<About />} />
 
-                 <Route path='/contactus' element={<Contact />} />
-          <Route
-          path="/usersignup"
-          element={<SignUp />}
-        />
+          <Route path="/contactus" element={<Contact />} />
+          
+          <Route path="/usersignup" element={<SignUp />} />
 
-        <Route
-          path="/login"
-          element={<Login />}
-        />
+          <Route path="/login" element={<Login />} />
 
-        <Route
-          path="/forgot-password"
-          element={<ForgotPassword />}
-        />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
 
-        <Route
-          path="/new-password"
-          element={<NewPassword />}
-        />
-      
+          <Route path="/new-password" element={<NewPassword />} />
+         
+          <Route path="/profile" element={<UserProfile />} />
+
         </Routes>
         <Footer />
       </Router>
     </div>
-
-  )
+  );
 }
-  export default App;
+export default App;
---
+++ src/pages/Auth/UserProfilePage.jsx
@@ -0,0 +1,12 @@
+import AuthLayout from "../../Shared/AuthLayout";
+import UserProfile from "../../Components/Auth/UserProfile/UserProfile";
+
+const UserProfilePage = () => {
+  return (
+    <AuthLayout>
+      <UserProfile />
+    </AuthLayout>
+  );
+};
+
+export default UserProfilePage;
